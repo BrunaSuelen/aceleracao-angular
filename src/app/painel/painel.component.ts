88287b41c67f898e87f83388a71d10449df12c13
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-07 14:26:22 -0300

    feat: Adiciona metodo para capturar mudanças do input e valida tradução

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,14 +10,41 @@ import { FRASES } from './frase-mock';
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
-  public instrucao: string = 'Traduza a frase'
+  public instrucao: string = 'Traduza a frase';
+  public resposta!: string;
+  public progresso: number = 0;
+
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
 
   constructor() {
-    console.log(this.frases)
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase)
   }
 
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    console.log(this.resposta)
   }
 
+  public verificarResposta(): void {
+    if (this.resposta && this.frasesIguais()) {
+      this.rodada++;
+      this.progresso += (100 / this.frases.length);
+      this.rodadaFrase = this.frases[this.rodada];
+      alert('A tradução está correta!')
+      return
+    }
+
+    alert('A tradução está errada!')
+  }
+
+  private frasesIguais(): boolean {
+    let frase = this.rodadaFrase.frasePtBr.split(' ').join('').toLocaleUpperCase();
+    let resposta = this.resposta.split(' ').join('').toLocaleUpperCase()
+
+    return frase == resposta
+  }
 }

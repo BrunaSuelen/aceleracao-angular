3aab206347cbd64d76bc75fa6287d0939fdb7481
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-07 10:05:14 -0300

    feat: Recebe e renderiza dados da frase dinamicamente

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frase-mock';
 
 @Component({
   selector: 'app-painel',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a frase'
+
+  constructor() {
+    console.log(this.frases)
+  }
+
 
   ngOnInit(): void {
   }

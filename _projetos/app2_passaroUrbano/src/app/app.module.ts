b41660396bb33d91b8148a7524357af5b0356f4d
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-08 15:08:55 -0300

    feat: Importa novos componentes no modulo

--- _projetos/app2_passaroUrbano/src/app/app.module.ts
+++ _projetos/app2_passaroUrbano/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    RodapeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

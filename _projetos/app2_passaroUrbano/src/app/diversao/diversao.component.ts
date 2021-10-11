192de7c43f66f579419678e89fa5d609ad10b291
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-11 17:18:50 -0300

    fix: Lista de frma dinamica as ofertas de diversao

--- _projetos/app2_passaroUrbano/src/app/diversao/diversao.component.ts
+++ _projetos/app2_passaroUrbano/src/app/diversao/diversao.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/ofertas.model';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [OfertasService]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas!: Oferta[];
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertasService.getOfertasPorCategoria('diversao')
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+        console.log(this.ofertas)
+      })
   }
-
 }

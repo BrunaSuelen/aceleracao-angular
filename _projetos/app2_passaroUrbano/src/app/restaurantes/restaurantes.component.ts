3f5386e9bcb61fcf73954182ae02cca331a020cd
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-11 16:31:30 -0300

    feat: Adiciona dinamismo na lista de restaurantes

--- _projetos/app2_passaroUrbano/src/app/restaurantes/restaurantes.component.ts
+++ _projetos/app2_passaroUrbano/src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/ofertas.model';
 
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  styleUrls: ['./restaurantes.component.css'],
+  providers: [OfertasService]
 })
 export class RestaurantesComponent implements OnInit {
 
-  constructor() { }
+  public ofertas!: Oferta[];
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertasService.getOfertasPorCategoria('restaurante')
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+        console.log(this.ofertas)
+      })
   }
-
 }

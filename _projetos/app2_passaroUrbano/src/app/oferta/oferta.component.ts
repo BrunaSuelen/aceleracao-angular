65b2a872e29a3ca221efe25b548e5a9ea5ad4945
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-13 10:46:54 -0300

    feat: Aciona requisição e renderiza dados recuperados

--- _projetos/app2_passaroUrbano/src/app/oferta/oferta.component.ts
+++ _projetos/app2_passaroUrbano/src/app/oferta/oferta.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/ofertas.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public oferta!: Oferta;
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) { }
 
   ngOnInit(): void {
-    this.route.snapshot.params['id']
+    // this.route.params.subscribe((parametro: any) => {
+    //   console.log(parametro)
+    // });
+
+    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta;
+      })
   }
 
 }

ee3fd8610156eb7d6e0866905b724774a2e955c3
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-13 12:06:06 -0300

    feat: importação de novos componentes

--- _projetos/app2_passaroUrbano/src/app/app.module.ts
+++ _projetos/app2_passaroUrbano/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
 import { OfertaComponent } from './oferta/oferta.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { OfertaComponent } from './oferta/oferta.component';
     HomeComponent,
     RestaurantesComponent,
     DiversaoComponent,
-    OfertaComponent
+    OfertaComponent,
+    ComoUsarComponent,
+    OndeFicaComponent
   ],
   imports: [
     BrowserModule,

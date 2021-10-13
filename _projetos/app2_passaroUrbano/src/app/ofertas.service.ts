513eb752933048aa23dc05ad3161b7723aa519b2
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-13 11:31:09 -0300

    fix: Adiciona url api em arquivo separado

--- _projetos/app2_passaroUrbano/src/app/ofertas.service.ts
+++ _projetos/app2_passaroUrbano/src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Oferta } from './shared/ofertas.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { URL_API } from './shared/app.api';
 
 @Injectable()
 export class OfertasService {
@@ -8,19 +9,19 @@ export class OfertasService {
   constructor(private http: HttpClient) { }
 
   public getOfertas(): Promise<Oferta[]> {
-    return this.http.get('http://localhost:3000/ofertas?destaque=true')
+    return this.http.get(`${URL_API}?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta)
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta)
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
-    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+    return this.http.get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => resposta.shift())
   }

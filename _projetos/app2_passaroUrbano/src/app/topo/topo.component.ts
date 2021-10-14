67b20d9e05c182f3fe09e5368d2dc7af41eced5e
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-14 11:59:03 -0300

    feat: Utilização e tratamento para a requisição de pesuisa utilizando o subject

--- _projetos/app2_passaroUrbano/src/app/topo/topo.component.ts
+++ _projetos/app2_passaroUrbano/src/app/topo/topo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/ofertas.model';
 
@@ -13,19 +14,43 @@ export class TopoComponent implements OnInit {
 
   public ofertas!: Observable<Oferta[]>
 
+  private subjectPesquisa: Subject<string> = new Subject<string>()
+
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa
+      // Aciona o subscribe após 1000 milisegundos
+      .pipe(debounceTime(1000))
+      // Aciona o subscribe apenas se o valor enviado ser diferente do anterior
+      .pipe(distinctUntilChanged())
+      // Trata dado enviado e retorna valor tratado
+      .pipe(switchMap((termo: string) => {
+        console.log('pesquisa http', termo)
+        return termo.trim() == ''
+          ? of<Oferta[]>([])
+          : this.ofertasService.pesquisaOfertas(termo)
+      }))
+      // Tratamento caso tenha error
+      .pipe(catchError((erro: any) => {
+        return of<Oferta[]>([])
+      }))
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      console.log('ofertas', ofertas)
+    })
   }
 
   public pesquisa(termoDaPesquisa: string): void {
-    console.log(termoDaPesquisa)
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaPesquisa);
+    this.subjectPesquisa.next(termoDaPesquisa.trim())
+    console.log('keyup', termoDaPesquisa)
 
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (error: any) => console.log(error),
-      () => console.log('Fluxo de eventos completo')
-    )
+    // this.ofertas = this.ofertasService.pesquisaOfertas(termoDaPesquisa);
+
+    // this.ofertas.subscribe(
+    //   (ofertas: Oferta[]) => console.log(ofertas),
+    //   (error: any) => console.log(error),
+    //   () => console.log('Fluxo de eventos completo')
+    // )
   }
 }

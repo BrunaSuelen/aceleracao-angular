3afaf1419482b9b0ea336d0eeb50a87fb5965ce3
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-15 09:55:27 -0300

    feat: Adiciona estrutura e validações

--- _projetos/app2_passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ _projetos/app2_passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = '';
+  public numero: string = '';
+  public complemento: string = '';
+  public formaPagamento: string = '';
+
+  public enderecoValido!: boolean;
+  public numeroValido!: boolean;
+  public complementoValido!: boolean;
+  public formaPagamentoValido!: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  atualizaEndereco(endereco: string) {
+    this.endereco = endereco;
+    this.enderecoValido = this.endereco.length > 3;
+    console.log(this.endereco);
+  }
+
+  atualizaNumero(numero: string) {
+    this.numero = numero;
+    this.numeroValido = this.numero.length > 0 ;
+    console.log(this.numero);
+  }
+
+  atualizaComplemento(complemento: string) {
+    this.complemento = complemento;
+    this.complementoValido = this.complemento.length > 0;
+    console.log(this.complemento);
+  }
+
+  atualizaFormaPagamento(formaPagamento: string) {
+    this.formaPagamento = formaPagamento;
+    this.formaPagamentoValido = this.formaPagamento.length > 0;
+    console.log(this.formaPagamento);
   }
 
 }

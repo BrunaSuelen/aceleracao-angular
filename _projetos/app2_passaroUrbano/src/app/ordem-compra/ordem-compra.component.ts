001f1b36f37fe75a32775daf0986cf40ad479442
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-18 12:09:11 -0300

    feat: Implementa request para criar novo pedido

--- _projetos/app2_passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ _projetos/app2_passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 
@@ -6,13 +7,31 @@ import { Pedido } from '../shared/pedido.model'
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [ OrdemCompraService ]
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
+
+
+  @ViewChild('formulario') public formulario!: NgForm;
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
-    
+
+  }
+
+  public confirmarCompra(): void {
+    let pedido: Pedido = new Pedido(
+      this.formulario.value.endereco,
+      this.formulario.value.numero,
+      this.formulario.value.complemento,
+      this.formulario.value.formaPagamento
+    );
+    console.log(pedido)
+
+    this.ordemCompraService.efetivarCompra(pedido)
+      .subscribe((idPedido: number) => {
+        console.log('Pedido cadastrado com sucesso ID do pedido', idPedido)
+      })
   }
 }
